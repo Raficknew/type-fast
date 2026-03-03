99be7a208eb8a47447f07ac336b7dda1d2476964
Raficknew <rafi20302@gmail.com>
2026-03-03 18:10:31 +0100

    feat: added sentences and game logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div>Start 17:00</div>;
+"use client";
+import { TypeTest } from "@/components/TypeTest";
+import { sentences } from "@/lib/sentences";
+import { useState } from "react";
+
+export default function TypeFastGamePage() {
+  const [hasGameStarted, setHasGameStarted] = useState(false);
+  const [sentence, setSentence] = useState<string>("");
+
+  const startGame = () => {
+    setHasGameStarted(true);
+    const randomSentence =
+      sentences[Math.floor(Math.random() * sentences.length)];
+    setSentence(randomSentence);
+  };
+
+  if (!hasGameStarted && !sentence) {
+    return (
+      <button
+        className="bg-blue-400 px-10 py-4 rounded-sm text-white text-2xl"
+        type="button"
+        onClick={startGame}
+      >
+        Start Game
+      </button>
+    );
+  }
+
+  return <TypeTest sentence={sentence} />;
 }
---
+++ src/components/TypeTest.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { sentences } from "@/lib/sentences";
+import { useEffect, useRef, useState } from "react";
+
+export function TypeTest({ sentence }: { sentence: string }) {
+  const [currentSentence, setCurrentSentence] = useState<string>(sentence);
+  const roundTime = 60; // seconds
+  const wordsInSentence = currentSentence.split(" ");
+  const charCounter = currentSentence.length;
+
+  const [currentText, setCurrentText] = useState<string>("");
+  const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
+  const [correctWordsCount, setCorrectWordsCount] = useState<number>(0);
+  const [counter, setCounter] = useState<number>(roundTime);
+  const [mistakes, setMistakes] = useState<number>(0);
+
+  const [hasRoundEnded, setHasRoundEnded] = useState<boolean>(false);
+  const WPM = useRef(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCounter((prevCounter) => {
+        if (prevCounter <= 1) {
+          clearInterval(timer);
+          setHasRoundEnded(true);
+          return 0;
+        }
+        return prevCounter - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!hasRoundEnded && correctWordsCount > 0) {
+      WPM.current = Math.round(
+        (correctWordsCount / (roundTime - counter)) * 60,
+      );
+    }
+  }, [hasRoundEnded, correctWordsCount, counter]);
+
+  const handleWordCheck = (text: string) => {
+    const isCorrect = text.trim() === wordsInSentence[currentWordIndex];
+    const isLastWord = currentWordIndex === charCounter - 1;
+
+    if (isCorrect) {
+      setCorrectWordsCount((prev) => prev + 1);
+      setCurrentWordIndex((prev) => prev + 1);
+      setCurrentText("");
+      if (isLastWord) {
+        setHasRoundEnded(true);
+      }
+    } else {
+      setMistakes((prev) => prev + 1);
+    }
+  };
+
+  const handleInputChange = (text: string) => {
+    setCurrentText(text);
+
+    if (text.endsWith(" ")) {
+      handleWordCheck(text);
+    } else {
+      for (let i = 0; i < text.length; i++) {
+        if (text[i] !== wordsInSentence[currentWordIndex][i]) {
+          setMistakes((prev) => prev + 1);
+          break;
+        }
+      }
+    }
+  };
+
+  const calculateAccuracy = () => {
+    if (charCounter === 0) return 100;
+    return Math.round(((charCounter - mistakes) / charCounter) * 100);
+  };
+
+  return (
+    <div className="flex flex-col gap-2 p-4 max-w-125">
+      <div className="text-center text-2xl">Next Round in: {counter}</div>
+      <div className="text-xl mb-4 bg-gray-100 p-2 rounded-sm">
+        {currentSentence}
+      </div>
+      <input
+        type="text"
+        className="border rounded-sm p-1 w-full"
+        value={currentText}
+        onChange={(e) => handleInputChange(e.target.value)}
+        disabled={hasRoundEnded}
+      />
+      <div>
+        <h3>WPM: {WPM.current}</h3>
+        <h3>Accuracy: {calculateAccuracy()}%</h3>
+      </div>
+    </div>
+  );
+}
